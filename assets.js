3cb6289c063758f107c447113bd3a3ad11287c25
Matalytic <matthewaarongo@gmail.com>
2017-04-02 18:04:49 -0400

    v2

--- assets.js
+++ assets.js
@@ -1,10 +1,20 @@
-// Code goes here
 var todoList = {
   
 	todos: [{todoText: "item 1", completed: false},{todoText: "item 2", completed: false},{todoText: "item 3", completed: false}],
 	
+	// displays entire todo list
   displayTodos: function() {
-    console.log("My Todos", this.todos);
+    if (this.todos.length ===0) {
+			console.log("Your todo list is empty");
+		} else {
+			for (var i = 0; i < this.todos.length; i++) {
+				if (this.todos[i].completed === true) {
+					console.log('(x)', this.todos[i].todoText);
+				} else {
+					console.log('( )', this.todos[i].todoText);
+				}
+			}
+		}	
   },
 	
   addTodo: function (todoText) {
@@ -24,6 +34,12 @@ var todoList = {
 	
 	deleteTodos: function (position) {
 		this.todos.splice(position, 1);
+		this.displayTodos();
+	},
+	
+	toggleCompleted: function(position) {
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
 		this.displayTodos();
 	}
 	
